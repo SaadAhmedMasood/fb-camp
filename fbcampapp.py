6d3a15c03c3e1c96cfe10b17227a21e661266e2b
Saad Ahmed Masood <145034934+SaadAhmedMasood@users.noreply.github.com>
2024-03-12 22:58:11 +0500

    Update fbcampapp.py

--- fbcampapp.py
+++ fbcampapp.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
-st.title('Live Prediction Of Expected Conversion test3')
+st.title('Live Prediction Of Expected Conversion test4')
 
-df = pd.read_excel('Social_FB.xlsx')
-#df = pd.read_csv('Social_FB.csv')
+#df = pd.read_excel('Social_FB.xlsx')
+df = pd.read_csv('Social_FB.csv')
 
 df
