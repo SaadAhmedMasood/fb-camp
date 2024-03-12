f3662eacca49c28b8f6dc819892b8e77f9e977c0
Saad Ahmed Masood <145034934+SaadAhmedMasood@users.noreply.github.com>
2024-03-12 23:22:10 +0500

    Update fbcampapp.py

--- fbcampapp.py
+++ fbcampapp.py
@@ -14,3 +14,30 @@ st.title('Live Prediction Of Expected Conversion test7 sam')
 
 df = pd.read_excel('Social_FB.xlsx')
 df
+
+X = df.drop(columns=['approved_conversion'])
+y = df['approved_conversion']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+categorical_features = ['gender', 'ad_region']
+
+
+categorical_features = [feature for feature in categorical_features if feature in X.columns]
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(), categorical_features)
+    ])
+preprocessor
+
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('classifier', DecisionTreeClassifier(random_state=42))])
+
+pipeline.fit(X_train, y_train)
+
+y_pred = pipeline.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)*100
+print("Accuracy:", accuracy)
