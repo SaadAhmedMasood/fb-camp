53d36924142bff6844cc129fd79a6c2d507f2187
Saad Ahmed Masood <145034934+SaadAhmedMasood@users.noreply.github.com>
2024-03-12 23:03:41 +0500

    Update fbcampapp.py

--- fbcampapp.py
+++ fbcampapp.py
@@ -1,8 +1,16 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import plot_tree
 import streamlit as st
-st.title('Live Prediction Of Expected Conversion test4')
+import random
 
-#df = pd.read_excel('Social_FB.xlsx')
-df = pd.read_csv('Social_FB.csv')
 
+st.title('Live Prediction Of Expected Conversion test5 sam')
+
+df = pd.read_excel('Social_FB.xlsx')
 df
